ee0c93c69783a1edb255b314d94ef33aaff75261
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-29 16:16:01 +0300

    minor: small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { ArrowUpRightIcon, LinkedInIcon } from "@/components/icons";
+
 export default function Home() {
   return (
     <>
-      <p>Hello, world!</p>
+      <main className="mx-auto md:pt-24 pt-16 max-w-screen-sm container relative">
+        <div className="flex items-center gap-2">
+          <h1 className="font-semibold text-white">Osman Tunahan ARIKAN</h1>
+
+          <Link
+            href="https://www.linkedin.com/in/osmantunahan/"
+            target="_blank"
+            className="text-zinc-200 hover:text-white transition-colors"
+          >
+            <LinkedInIcon className="size-4" />
+          </Link>
+        </div>
+      </main>
     </>
   );
 }
