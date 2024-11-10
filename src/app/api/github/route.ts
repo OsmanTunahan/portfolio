6c3a97700a517fc0014bc7c90a7966a45918dff1
OsmanTunahan <osmantunahan007@gmail.com>
2024-11-10 12:33:50 +0300

    feat: add github stats (#3)

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -28,10 +28,67 @@ export async function GET() {
       .sort((a: IRepo, b: IRepo) => b.stargazers_count - a.stargazers_count)
       .filter(
         (repo: IRepo) => repo.owner.login === process.env.GITHUB_USERNAME!
-      ).slice(0, 5);
+      )
+      .slice(0, 5);
 
-    return NextResponse.json({ status: true, data: repos });
+    let getUserStats = await GET_STATS();
+
+    return NextResponse.json({
+      status: true,
+      repos,
+      stats: getUserStats?.data,
+    });
   } catch (error: any) {
     return NextResponse.json({ status: false, error: error.message });
+  }
+}
+
+async function GET_STATS() {
+  const username = process.env.GITHUB_USERNAME!;
+  const urls = [
+    `https://api.github.com/users/${username}`,
+    `https://api.github.com/search/commits?q=author:${username}`,
+    `https://api.github.com/search/issues?q=author:${username}+type:pr`,
+    `https://api.github.com/users/${username}/repos`,
+  ];
+
+  try {
+    const [userResponse, commitsResponse, prsResponse, reposResponse] =
+      await Promise.all(
+        urls.map((url) =>
+          fetch(url, {
+            headers: {
+              Accept: "application/vnd.github.v3+json",
+            },
+          })
+        )
+      );
+
+    const user = await userResponse.json();
+    const commits = await commitsResponse.json();
+    const prs = await prsResponse.json();
+    const repos = await reposResponse.json();
+
+    const totalStars = repos.reduce(
+      (acc: number, repo: IRepo) => acc + repo.stargazers_count,
+      0
+    );
+
+    const stats = {
+      followers: user.followers,
+      totalCommits: commits.total_count,
+      totalPRs: prs.total_count,
+      totalStars,
+    };
+
+    return {
+      status: true,
+      data: stats,
+    };
+  } catch (error: any) {
+    return {
+      status: false,
+      error: error.message,
+    };
   }
 }
