d601a44bd4ad7a6d8bbc389e534625f260de8d37
OsmanTunahan <osmantunahan007@gmail.com>
2024-11-12 22:21:43 +0300

    refactor: improve auth providers (#13)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,26 @@
+import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-const handler = NextAuth({
+const clientId = process.env.GITHUB_ID;
+const clientSecret = process.env.GITHUB_SECRET;
+
+if (!clientId || !clientSecret) {
+  throw new Error(
+    "Please define GITHUB_ID and GITHUB_SECRET environment variables"
+  );
+}
+
+const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET!,
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    })
+      clientId,
+      clientSecret,
+    }),
   ],
-  session: { strategy: "jwt" },
-  callbacks: {
-    async signIn(userDetail) {
-      if (Object.keys(userDetail).length === 0) {
-        return false;
-      }
-      return true;
-    },
-    async redirect({ baseUrl }) {
-      return `${baseUrl}/session-check`;
-    },
-    async session({ session, token }) {
-      if (session.user?.name) session.user.name = token.name;
-      return session;
-    },
-    async jwt({ token, user }) {
-      let newUser = { ...user } as any;
-      if (newUser.first_name && newUser.last_name)
-        token.name = `${newUser.first_name} ${newUser.last_name}`;
-      return token;
-    },
-  },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
