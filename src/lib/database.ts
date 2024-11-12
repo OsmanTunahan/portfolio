9e87990607260a0e59413eb4d4a920ef8126a56a
OsmanTunahan <osmantunahan007@gmail.com>
2024-11-12 13:58:33 +0300

    feat: add database connection (#9)

---
+++ src/lib/database.ts
@@ -0,0 +1,31 @@
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+
+const options = { dbName: "Portfolio" };
+
+let clientPromise: Promise<Mongoose>;
+
+if (process.env.NODE_ENV === "development") {
+  let globalMongo = global as typeof globalThis & {
+    isConnected?: Promise<Mongoose>;
+  };
+
+  if (!globalMongo.isConnected) {
+    globalMongo.isConnected = mongoose.connect(uri, options as ConnectOptions);
+  }
+  clientPromise = globalMongo.isConnected;
+} else {
+  try {
+    clientPromise = mongoose.connect(uri, options as ConnectOptions);
+  } catch (err) {
+    console.log(err);
+  }
+  clientPromise = mongoose.connect(uri, options as ConnectOptions);
+}
+
+export default clientPromise;
