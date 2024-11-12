daeb669ce9283ad4b646bd34c1345ded01a76c0c
Osman Tunahan ARIKAN <154083945+OsmanTunahan@users.noreply.github.com>
2024-11-12 16:27:48 +0300

    feat: add github provider (#13)

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async signIn(userDetail) {
+      if (Object.keys(userDetail).length === 0) {
+        return false;
+      }
+      return true;
+    },
+    async redirect({ baseUrl }) {
+      return `${baseUrl}/session-check`;
+    },
+    async session({ session, token }) {
+      if (session.user?.name) session.user.name = token.name;
+      return session;
+    },
+    async jwt({ token, user }) {
+      let newUser = { ...user } as any;
+      if (newUser.first_name && newUser.last_name)
+        token.name = `${newUser.first_name} ${newUser.last_name}`;
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
